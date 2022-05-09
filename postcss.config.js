39e47fd031a4941a291ce5ebaaa0ae337fac0461
Abdul Elsadig <abdelrahmanelsadig1@gmail.com>
2022-05-09 11:37:01 +0200

    initial commit

---
+++ postcss.config.js
@@ -0,0 +1,8 @@
+const tailwindcss = require("tailwindcss");
+const autoprefixer = require("autoprefixer");
+const cssnano = require("cssnano");
+
+
+module.exports = {
+	plugins: [cssnano, autoprefixer, tailwindcss],
+}
